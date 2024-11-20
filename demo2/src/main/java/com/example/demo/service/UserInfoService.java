1a074399f1ee6a4b886acfcbb91afd23f0706c9d
TrinhHuuTho <trinhuutho@gmail.com>
2024-11-20 15:59:58 +0700

    Update bai 2

--- demo2/src/main/java/com/example/demo/service/UserInfoService.java
+++ demo2/src/main/java/com/example/demo/service/UserInfoService.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.config.UserInfoUserDetails;
 import com.example.demo.entities.UserInfor;
 import com.example.demo.repository.UserInforRepository;
 
+@Service
 public class UserInfoService implements UserDetailsService {
 
 	@Autowired
